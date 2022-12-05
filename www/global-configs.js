20f8303780ca44a59cedaf5dc863db3147843d4f
Plurilogic <savoier@plurilogic.com>
2022-12-05 13:30:52 -0500

    19.5

--- www/global-configs.js
+++ www/global-configs.js
@@ -3,8 +3,8 @@ var globalConfigs = {
   serializer: 'urlencoded',
   followRedirect: true,
   timeout: 60.0,
-  connectTimeout: 30.0,
-  readTimeout: 30.0
+  connectTimeout: 19.5,
+  readTimeout: 19.5
 };
 
 module.exports = globalConfigs;
